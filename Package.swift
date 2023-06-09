c4f45af2b57c7427128af1eb3a97c1201586d537
Julia Nikitina <joliejuly@gmail.com>
2023-06-09 15:15:45 +0300

    Add resources

--- Package.swift
+++ Package.swift
@@ -15,9 +15,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DesignColors"),
+            name: "DesignColors",
+            resources: [.process("Colors")]
+        ),
         .testTarget(
             name: "DesignColorsTests",
             dependencies: ["DesignColors"]),
+        
     ]
 )
